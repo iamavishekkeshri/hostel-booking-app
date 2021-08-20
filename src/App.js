afe4263c4b9926c4c88af377b075694608966930
Abhishek keshri <abhishekkeshri@live.com>
2021-08-20 18:17:59 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import Topbar from './Topbar';
+import SuccessPage from './SuccessPage';
+import Homepage from './Homepage';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import LoginPage from './LoginPage';
+import React from 'react';
+import { ReactReduxContext } from 'react-redux';
+import Mainpage from './Mainpage';
+import ChooseGender from './ChooseGender';
+import ChooseHostel from './ChooseHostel';
+import ChooseFloor from './ChooseFloor';
+
+class App extends React.Component{
+  render(){
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route exact path={"/login"} component={LoginPage}/>
+            <Route path={'/main'} component={Mainpage}/>
+            <Route path={'/gender'} component={ChooseGender}/>
+            <Route path={'/hostel'} component={ChooseHostel}/>
+            <Route path={'/floor'} component={ChooseFloor}/>
+            <Route path={'/success'} component={SuccessPage}/>
+            <Route path={'/'} component={Homepage}/>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
+
+export default App;
